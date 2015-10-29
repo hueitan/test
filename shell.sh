6f62083f849e1f580177575315e41d6fddadc777
Not me <idontknow@idontknow.me>
2015-10-29 11:17:07 +0800

    Update shell

--- shell.sh
+++ shell.sh
@@ -1,9 +1,16 @@
 #!/bin/sh
 while [ true ]
 do
+    # master
+    git pull origin master
     python scrape.py
-    git add -A
+    # gh-pages
+    git checkout gh-pages
+    git pull origin gh-pages
+    git add restaurant.json
     git commit -m 'Update data'
-    git push origin master
+    git push origin gh-pages
+
+    # sleep 30
     sleep 30
 done
