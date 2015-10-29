f3da3a31f61489dfe850fcb037c8211d1c37dad0
Not me <idontknow@idontknow.me>
2015-10-29 15:06:43 +0800

    Update scrape

--- scrape.py
+++ scrape.py
@@ -5,15 +5,20 @@ import urllib
 import requests
 import json
 import io
+import os
+import sys
 from datetime import datetime
 from bs4 import BeautifulSoup
 
 # Restaurant
 restaurants = []
 
+# country
+country = sys.argv[1]
+
 # domain url
 domain = "http://www.ipeen.com.tw"
-url = domain + "/taipei/channel/F?p=1&id=F&so=cdate&soway=d"
+url = domain + "/" + country + "/channel/F?p=1&id=F&so=cdate&soway=d"
 
 # html content
 html = requests.get(url)
@@ -30,14 +35,17 @@ for link in firstObj:
     resObj = {"url": domain + temp["href"], "title": temp.text, "comment_id": temp["href"][9:], "created_time": created_time, "img_url": img_url}
     restaurants.append(resObj)
 
-# create a new json to data/
+# create a new json to data/[date]
 data = ""
-with io.open("data/"+(datetime.now()).strftime("%Y%m%d-%H:%M:%S")+".json", "w+", encoding="utf-8") as outfile:
+date = "data/" + (datetime.now()).strftime("%Y%m%d")
+if not os.path.exists(date):
+    os.makedirs(date)
+with io.open(date + "/" +country + "-" + (datetime.now()).strftime("%H:%M:%S")+".json", "w+", encoding="utf-8") as outfile:
     data = json.dumps(restaurants, ensure_ascii=False, indent=4)
     outfile.write(unicode(data))
 
-# overwrite restaurant.json
-with io.open("restaurant.json", "w+", encoding="utf-8") as outfile:
+# overwrite restaurant-[country].json
+with io.open("restaurant-" + country + ".json", "w+", encoding="utf-8") as outfile:
     outfile.write(unicode(data))
 
 # Output Save all Restaurant with Url
