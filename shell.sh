993f2a67512a55b227e2bce97863c232bf210cb8
Not me <idontknow@idontknow.me>
2015-10-29 15:19:00 +0800

    add more

--- shell.sh
+++ shell.sh
@@ -4,7 +4,14 @@ do
     # master
     git checkout master
     git pull origin master
-    python scrape.py
+    # scrape taipei
+    python scrape.py taipei
+    # scrape taichung
+    python scrape.py taichung
+    # scrape kaohsiung
+    python scrape.py kaohsiung
+    # scrape japan
+    python scrape.py japan
     git add -A
     git commit -m 'Update data'
     git push origin master
@@ -15,8 +22,8 @@ do
     # gh-pages
     git checkout gh-pages
     git pull origin gh-pages
-    git checkout master -- restaurant.json
-    git add restaurant.json
+    git checkout master -- restaurant-*.json
+    git add restaurant-*.json
     git commit -m 'Update data'
     git push origin gh-pages
 
